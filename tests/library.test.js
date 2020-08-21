3e6255fe233cbdd8b57eb26c6756d397b6f51bfc
dmondo <dannysmondo@gmail.com>
2020-08-21 13:53:39 -0700

    test(events): unit tests for event signalling

--- tests/library.test.js
+++ tests/library.test.js
@@ -1,6 +1,50 @@
 import EventLibrary from '../dist/library';
 
-test('library should instantiate without errors', () => {
-  const lib = new EventLibrary();
-  expect(lib).toBeInstanceOf(EventLibrary);
+const lib = new EventLibrary();
+
+describe('library should exist', () => {
+  test('library should instantiate without errors', () => {
+    expect(lib).toBeInstanceOf(EventLibrary);
+  });
+});
+
+describe('user may signal library about events', () => {
+  test('user can signal library that an event has occured', () => {
+    lib.signal('click');
+    expect(lib.summarize('click')).toBe(1);
+  });
+
+  test('user can signal that events of different names have occured', () => {
+    lib.signal('click');
+    lib.signal('req');
+    expect(lib.summarize('click')).toBe(2);
+    expect(lib.summarize('req')).toBe(1);
+  });
+
+  test('getting count for event that has not occured returns 0', () => {
+    expect(lib.summarize('post')).toBe(0);
+  });
+});
+
+describe('events are associated with an eventTime', () => {
+  test('user may override event time when signalling', () => {
+    lib.signal('change', new Date(0));
+    const allEvents = lib.getEvents();
+    expect(allEvents[allEvents.length - 1].eventTime.getTime()).toBe(0);
+  });
+
+  test('user may request events over a specified amount of time', () => {
+    const time1 = new Date();
+    time1.setSeconds(time1.getSeconds() - 12);
+    const time2 = new Date();
+    time2.setSeconds(time2.getSeconds() - 14);
+    const time3 = new Date();
+
+    lib.signal('update', time1);
+    lib.signal('update', time2);
+    lib.signal('update', time3);
+
+    expect(lib.summarize('update', 10)).toBe(1);
+    expect(lib.summarize('update')).toBe(3);
+  });
 });
